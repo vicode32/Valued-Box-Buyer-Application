69841f26930dd04928a280459298949bd319315b
pritamroy <pritam20002001@gmail.com>
2021-05-16 01:07:52 +0530

    empty_file

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ValuedBox_1"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/valuedbox_1/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.valuedbox_1
+
+expect class Platform() {
+    val platform: String
+}
