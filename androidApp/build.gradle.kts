0157b8b94246ef0170abd5dc7a8effd523eacb6f
pritamroy <pritam20002001@gmail.com>
2021-05-28 14:35:29 +0530

    base code

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 }
 
@@ -18,6 +18,7 @@ android {
         targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
+        vectorDrawables.useSupportLibrary = true
     }
     buildTypes {
         getByName("release") {
